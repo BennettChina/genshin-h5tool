c3a2f2e161f56f49636df905d82629c909e7237a
Bennett <w2500513053@outlook.com>
2022-04-29 15:49:41 +0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -4,6 +4,11 @@ import { privateClass } from "#genshin/init";
 import { getInfo } from "#genshin-h5tool/util/api";
 
 export async function main( { messageData, sendMessage }: InputParameter ): Promise<void> {
+	if ( new Date().getTime() >= new Date( '2022-04-29 GMT+8' ).getTime() ) {
+		await sendMessage( '活动已过期，请下次及时参与哦！' )
+		return;
+	}
+	
 	const userID: number = messageData.user_id;
 	const serID: number = parseInt( messageData.raw_message ) || 1;
 	const single: Private | string = await privateClass.getSinglePrivate( userID, serID );
